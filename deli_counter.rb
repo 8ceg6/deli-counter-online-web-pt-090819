348bc385820b0a5c7737a555b1d8bc1c44e20c86
cedric e goddard <8ceg6@comcast.net>
2019-09-30 10:29:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,8 @@ def line(array)
   if array.empty?
     puts "The line is currently empty."
   else
-    current_line = "This line is currently:"
-    array.each
+    current_line = "The line is currently:"
+    array.each.with_index(1) do |name,index|
+      current_line += " #{index}"
   end
 end  
